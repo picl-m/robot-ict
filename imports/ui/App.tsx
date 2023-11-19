4ac777bc21a3f4cd7bf842603f305bd007e5a6c7
Matouš Pícl <87586480+picl-m@users.noreply.github.com>
2023-11-19 16:16:36 +0100

    initial commit

---
+++ imports/ui/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Table } from './Table';
+
+export const App = () => (
+  <div>
+    <h1>RobotICT table</h1>
+    <Table/>
+  </div>
+);
---
+++ imports/ui/Table.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Table = () => (
+    <table>
+        <tr>
+            <td>asdf</td>
+            <td>fdsa</td>
+        </tr>
+        <tr>
+            <td>qwert</td>
+            <td>trewq</td>
+        </tr>
+    </table>
+);
