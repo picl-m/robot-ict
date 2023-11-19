3ee57f72d6d288a9e25be433bc28e3e3088f6133
Matouš Pícl <87586480+picl-m@users.noreply.github.com>
2023-11-19 18:34:17 +0100

    added table component logic

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Table } from './Table';
 
-export const App = () => (
-  <div>
-    <h1>RobotICT table</h1>
-    <Table/>
-  </div>
-);
+export const App = () => {
+  return(
+    <div>
+      <h1>RobotICT table</h1>
+      <Table minCount={1} maxCount={100} />
+    </div>
+  )
+};
--- imports/ui/Table.tsx
+++ imports/ui/Table.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
 
-export const Table = () => (
-    <table>
-        <tr>
-            <td>asdf</td>
-            <td>fdsa</td>
-        </tr>
-        <tr>
-            <td>qwert</td>
-            <td>trewq</td>
-        </tr>
-    </table>
-);
+interface TableProps {
+    minCount: number;
+    maxCount: number;
+}
+
+export const Table = (props: TableProps) => {
+    const getDivisionString = (number: number) => {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return("RobotICT");
+        } else if (number % 3 == 0) {
+            return("Robot");
+        } else if (number % 5 == 0) {
+            return("ICT");
+        }
+            
+        return(number);
+    }
+
+    const list = [];
+    for (let i = props.minCount; i <= props.maxCount; i++) {
+        list.push(
+            <tr key={i}>
+                <td>{getDivisionString(i)}</td>
+            </tr>
+        )
+    }
+
+    return(
+        <table>
+            <tbody>
+                {list}
+            </tbody>
+        </table>
+    );
+};
